da1ae1b3ca1477a848aae079f5b27b32fbaa0570
Bustervro <126031898+Bustervro@users.noreply.github.com>
2026-03-21 19:45:36 -0700

    Create sortedSquaredGrowth.js

---
+++ sortedSquaredGrowth.js
@@ -0,0 +1,27 @@
+function sortedSquaredGrowth(growthPercentages) {
+  const n = growthPercentages.length;
+  const result = new Array(n);
+
+  let left = 0;
+  let right = n - 1;
+  let writeIndex = n - 1;
+
+  while (left <= right) {
+    const leftSquare = growthPercentages[left] * growthPercentages[left];
+    const rightSquare = growthPercentages[right] * growthPercentages[right];
+
+    if (leftSquare > rightSquare) {
+      result[writeIndex] = leftSquare;
+      left++;
+    } else {
+      result[writeIndex] = rightSquare;
+      right--;
+    }
+
+    writeIndex--;
+  }
+
+  return result;
+}
+
+module.exports = sortedSquaredGrowth;
